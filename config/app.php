6725359035ea1fa109e4d84820270b5778defb3b
lutriveflina <lutriveflina29@gmail.com>
2024-10-02 08:39:08 +0700

    add https

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://laborkesehatan.bukittinggikota.go.id/'),
+    'url' => env('APP_URL', 'https://laborkesehatan.bukittinggikota.go.id/'),
 
-    'asset_url' => env('ASSET_URL', 'http://laborkesehatan.bukittinggikota.go.id/public'),
+    'asset_url' => env('ASSET_URL', 'https://laborkesehatan.bukittinggikota.go.id/public'),
 
     /*
     |--------------------------------------------------------------------------
