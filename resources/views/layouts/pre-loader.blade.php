ebb3d0f2a9b20ad258d8e42e250d046925814b7f
lutriveflina <lutriveflina29@gmail.com>
2025-11-12 15:01:00 +0700

    delete height

--- resources/views/layouts/pre-loader.blade.php
+++ resources/views/layouts/pre-loader.blade.php
@@ -1,7 +1,7 @@
 <div id="pre-loader">
     {{-- <div id="loader-logo" style="background-image: url({{ asset('/assets/image/Logo_Kota_Bukittinggi.png') }})">
     </div><!-- loader-logo --> --}}
-    <div style="position: relative; height: 100%;">
+    <div style="position: relative;">
         <img src="{{ asset('/assets/image/Logo_Kota_Bukittinggi.png') }}" alt="" style="" class="logo">
         <div id="loader-circle"></div><!-- loader-circle -->
     </div>
