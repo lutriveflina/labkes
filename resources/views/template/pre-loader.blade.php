0b3761069ff723eaf21cab9830be104ad342f802
lutriveflina <lutriveflina29@gmail.com>
2026-07-14 02:59:12 +0000

    update

--- resources/views/template/pre-loader.blade.php
+++ resources/views/template/pre-loader.blade.php
@@ -1,8 +1,8 @@
 <div id="pre-loader">
     {{-- <div id="loader-logo" style="background-image: url({{ asset('/assets/image/Logo_Kota_Bukittinggi.png') }})">
     </div><!-- loader-logo --> --}}
-    <div style="position: relative; height: 100%;">
-        <img src="{{ asset('/assets/image/Logo_Kota_Bukittinggi.png') }}" alt="" style="" class="logo">
+    <div style="position: relative;">
+     {{--   <img src="{{ asset('/assets/image/Logo_Kota_Bukittinggi.png') }}" alt="" style="" class="logo"> --}}
         <div id="loader-circle"></div><!-- loader-circle -->
     </div>
     <div class="loader-section section-left"></div><!-- loader-section -->
